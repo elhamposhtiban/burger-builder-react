7ee44a9e7d28f9faf76427a12379ff0c96d3e212
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-04-12 05:55:59 -0500

    adding component, container folders, layout, burgerbuilder, burger ingredients to the application

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Layout.css"
+
+const layout= (props) => 
+    (
+        <React.Fragment>
+
+        <div> Toolbar, SideDrawer, Backdrop </div>
+        <main className="content">
+             {props.children}
+        </main>
+        </React.Fragment>
+
+    )
+
+
+    export default layout;
